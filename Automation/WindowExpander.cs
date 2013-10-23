f3756f05ea62784c2935a0ee02eed24768d7b783
satrex <sat1501@gmail.com>
2013-10-23 10:23:57 +0900

    足りてないファイルを追加

---
+++ Automation/WindowExpander.cs
@@ -0,0 +1,25 @@
+﻿using FNF.WindowController;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Automation
+{
+    public static class WindowExpander
+    {
+        public static void DoAfterActivated(this Window target, Action action)
+        {
+            int minute = 60 * 1000;
+            target = target.WaitForActive(2 * minute);
+
+            if (target == null)
+            {
+                throw new ApplicationException(string.Format(@"{0}を待ちましたが、アクティブになりませんでした。", target.Text));
+            }
+
+            if (action != null)
+                action.Invoke();
+        }
+    }
+}
